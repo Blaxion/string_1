02a2ee82a1e281619cbb1fbd33dc6e2e1497fc51
Blaxion <khibaziev.f@gmail.com>
2021-04-16 00:38:41 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,25 @@
+// #1
+// let name = prompt("Please enter your name");
+// if (name !=null){
+//     console.log("bonjour",name);
+// }
+
+
+// #2
+// let age = prompt("Quel age as-tu?");
+// if (age !=null){
+//     console.log("Tu as",age,"ans");
+// }
+
+
+// #3
+// let fullname = prompt("Ton nom") + prompt("Ton prénom")
+// if (fullname !=null){
+//     console.log(fullname);
+// }
+
+// #4
+// let x = prompt("Ton prénom").toUpperCase()+" "+prompt("Ton nom");
+// if (x !=null){
+//     console.log( "Bonjour",x);
+// }
